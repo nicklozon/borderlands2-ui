fa7cfe2a0787b573c015e92b17c181a0e2cd0ca4
Nick Lozon <nicklozon@gmail.com>
2020-06-30 23:46:14 -0400

    fix icon

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,13 +5,14 @@ import { AddWeaponButton } from './add_weapon_button'
 import { GearButton } from './gear_button'
 import { BuildButton } from './build_button'
 import { AboutButton } from './about_button'
+import Logo from '../../../src/assets/logo.png'
 
 export class NavBar extends React.Component {
   render() {
     return (
       <Navbar fixedToTop>
         <Navbar.Group>
-          <Navbar.Heading><img src='./src/assets/logo.png' /></Navbar.Heading>
+          <Navbar.Heading><img src={Logo} /></Navbar.Heading>
           <Navbar.Heading><strong>Borderlands 2 Damage Calculator</strong></Navbar.Heading>
           <Navbar.Divider />
           <AddWeaponButton />
